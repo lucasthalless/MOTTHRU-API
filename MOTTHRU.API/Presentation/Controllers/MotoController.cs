10f7caf8ac41209dab481cc903fba646b1ad75aa
lucas <lucaswtds@gmail.com>
2025-05-25 11:59:23 -0300

    feature: add swagger annotations + oracle development connection string + fix dependencies versions

--- MOTTHRU.API/Presentation/Controllers/MotoController.cs
+++ MOTTHRU.API/Presentation/Controllers/MotoController.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using MOTTHRU.API.Application.Dtos;
 using MOTTHRU.API.Application.Interfaces;
+using MOTTHRU.API.Domain.Entities;
+using Swashbuckle.AspNetCore.Annotations;
 using MOTTHRU.API.Domain.Interfaces;
 
 namespace MOTTHRU.API.Controllers
@@ -13,6 +16,13 @@ namespace MOTTHRU.API.Controllers
         private readonly IMotoApplicationService _motoApplicationService;
         
         [HttpGet]
+        [SwaggerOperation(
+            Summary = "Obter todas as motos",
+            Description = "")]
+        [SwaggerResponse(200, "Moto retornada com sucesso", typeof(List<MotoEntity>))]
+        [SwaggerResponse((int)HttpStatusCode.NoContent, "Moto não encontrada")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Falha para obter as motos")]
+
         public IActionResult Get()
         {
             try
@@ -31,6 +41,13 @@ namespace MOTTHRU.API.Controllers
         }
         
         [HttpGet("{id}")]
+        [SwaggerOperation(
+            Summary = "Obter moto por ID",
+            Description = "Retorna os dados de uma moto específica, com base no ID informado"
+        )]
+        [SwaggerResponse(200, "Moto retornada com sucesso", typeof(MotoEntity))]
+        [SwaggerResponse((int)HttpStatusCode.NoContent, "Moto não encontrada")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao obter a moto")]
         public IActionResult GetById(int id)
         {
             try
@@ -49,6 +66,13 @@ namespace MOTTHRU.API.Controllers
         }
         
         [HttpGet("byPatio")]
+        [SwaggerOperation(
+            Summary = "Obter motos por ID do pátio",
+            Description = "Retorna uma lista de motos associadas ao ID do pátio informado"
+        )]
+        [SwaggerResponse(200, "Motos retornadas com sucesso", typeof(List<MotoEntity>))]
+        [SwaggerResponse((int)HttpStatusCode.NoContent, "Nenhuma moto encontrada para o ID do pátio informado")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao obter as motos")]
         public IActionResult GetByIdPatio([FromQuery] string idPatio)
         {
             try
@@ -66,6 +90,14 @@ namespace MOTTHRU.API.Controllers
         }
         
         [HttpGet("byStatus")]
+        
+        [SwaggerOperation(
+            Summary = "Obter motos por status",
+            Description = "Retorna uma lista de motos com o status informado"
+        )]
+        [SwaggerResponse(200, "Motos retornadas com sucesso", typeof(List<MotoEntity>))]
+        [SwaggerResponse((int)HttpStatusCode.NoContent, "Nenhuma moto com o status informado")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao obter as motos")]
         public IActionResult GetByStatus([FromQuery] string status)
         {
             try
@@ -83,6 +115,12 @@ namespace MOTTHRU.API.Controllers
         }
         
         [HttpPost]
+        [SwaggerOperation(
+            Summary = "Cadastrar nova moto",
+            Description = "Cria uma nova moto no sistema com base nos dados enviados"
+        )]
+        [SwaggerResponse(201, "Moto criada com sucesso", typeof(MotoEntity))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao criar a moto")]
         public IActionResult Post([FromBody] MotoDto entity)
         {
             try
@@ -99,6 +137,12 @@ namespace MOTTHRU.API.Controllers
 
         
         [HttpPut("{id}")]
+        [SwaggerOperation(
+            Summary = "Atualizar moto existente",
+            Description = "Atualiza os dados de uma moto já cadastrada com base no ID"
+        )]
+        [SwaggerResponse(200, "Moto atualizada com sucesso", typeof(MotoEntity))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao atualizar a moto")]
         public IActionResult Put(int id, [FromBody] MotoDto entity)
         {
             try
@@ -114,6 +158,12 @@ namespace MOTTHRU.API.Controllers
         }
 
         [HttpDelete("{id}")]
+        [SwaggerOperation(
+            Summary = "Deletar moto",
+            Description = "Remove uma moto do sistema com base no ID informado"
+        )]
+        [SwaggerResponse(200, "Moto removida com sucesso")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Erro ao remover a moto")]
         public IActionResult Delete(int id)
         {
             try
